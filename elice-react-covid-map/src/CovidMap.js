ddf259a503bf7dbca12794d6ddd8c014ef45566e
[2기-1반] 김진경 <kimradium@naver.com>
2021-08-09 14:22:26 +0900

    1번쨰 커밋

---
+++ elice-react-covid-map/src/CovidMap.js
@@ -0,0 +1,133 @@
+import { useEffect, useState } from "react";
+import {
+  Seoul,
+  Gyeonggi,
+  Incheon,
+  Gangwon,
+  Chungnam,
+  Chungbuk,
+  Sejong,
+  Daejeon,
+  Gyeongnam,
+  Gyeongbuk,
+  Jeonbuk,
+  Jeonnam,
+  Ulsan,
+  Busan,
+  Daegu,
+  Gwangju,
+  Jeju,
+} from "./area/all_area";
+
+//
+
+import axios from "axios";
+
+const fillColor = ["#4088da", "#ffb911", "#fc7001", "#e60000"];
+
+function CovidMap() {
+  const [covidData, setCovidData] = useState(null);
+  const [selectData, setSelectData] = useState(null);
+
+  useEffect(() => {
+    console.log("업데이트 된 항목", covidData);
+  }, [covidData]);
+
+  useEffect(() => {
+    axios.post("http://localhost:5000/covidData").then((response) => {
+      // 요청 성공 시 실행되는 코드
+      setCovidData(response.data);
+    });
+  }, []);
+
+  function handleClick(e) {
+    const area = e.target.id;
+    setSelectData({
+      area,
+      num: covidData.data[area].num,
+      level: covidData.data[area].level
+    })
+  }
+
+  return covidData === null ? (
+    <div>Loading</div>
+  ) : (
+    <div>
+      {selectData && <h1>{selectData.area}</h1>}
+      {selectData && <p>확진자 수 : {selectData.num}</p>}
+      {selectData && <p>거리두기 단계 : {selectData.level}</p>}
+      <p>{covidData.updated_data} (기준)</p>
+      <svg width="700px" height="1000px" viewBox="0 0 800 1200">
+        <Seoul
+          fill={fillColor[covidData.data["서울"].level - 1]}
+          onClick={handleClick}
+        />
+        <Gyeonggi
+          fill={fillColor[covidData.data["경기"].level - 1]}
+          onClick={handleClick}
+        />
+        <Gangwon
+          fill={fillColor[covidData.data["강원"].level - 1]}
+          onClick={handleClick}
+        />
+        <Incheon
+          fill={fillColor[covidData.data["인천"].level - 1]}
+          onClick={handleClick}
+        />
+        <Chungnam
+          fill={fillColor[covidData.data["충남"].level - 1]}
+          onClick={handleClick}
+        />
+        <Chungbuk
+          fill={fillColor[covidData.data["충북"].level - 1]}
+          onClick={handleClick}
+        />
+        <Sejong
+          fill={fillColor[covidData.data["세종"].level - 1]}
+          onClick={handleClick}
+        />
+        <Daejeon
+          fill={fillColor[covidData.data["대전"].level - 1]}
+          onClick={handleClick}
+        />
+        <Gyeongnam
+          fill={fillColor[covidData.data["경남"].level - 1]}
+          onClick={handleClick}
+        />
+        <Gyeongbuk
+          fill={fillColor[covidData.data["경북"].level - 1]}
+          onClick={handleClick}
+        />
+        <Jeonbuk
+          fill={fillColor[covidData.data["전북"].level - 1]}
+          onClick={handleClick}
+        />
+        <Jeonnam
+          fill={fillColor[covidData.data["전남"].level - 1]}
+          onClick={handleClick}
+        />
+        <Ulsan
+          fill={fillColor[covidData.data["울산"].level - 1]}
+          onClick={handleClick}
+        />
+        <Busan
+          fill={fillColor[covidData.data["부산"].level - 1]}
+          onClick={handleClick}
+        />
+        <Daegu
+          fill={fillColor[covidData.data["대구"].level - 1]}
+          onClick={handleClick}
+        />
+        <Gwangju
+          fill={fillColor[covidData.data["광주"].level - 1]}
+          onClick={handleClick}
+        />
+        <Jeju
+          fill={fillColor[covidData.data["제주"].level - 1]}
+          onClick={handleClick}
+        />
+      </svg>
+    </div>
+  );
+}
+export default CovidMap;
